0e3092a131dbb871ea453ceb45c70324ead2ba62
ashiqurJRahman <19103006@iubat.edu>
2021-11-30 15:07:57 +0600

    project file

---
+++ client-site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from "./components/Home/Home";
+import {Navbar} from "react-bootstrap";
+
+
+function App() {
+    return (
+       <div>
+           <Home></Home>
+       </div>
+    );
+}
+
+export default App;
---
+++ client-site/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navber from "../Navber/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+        </div>
+    );
+};
+
+export default Home;
